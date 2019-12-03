279bbb14db5837d3f6a2fa280fd72c9fd69ecaab
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2019-12-03 12:33:44 +0700

    Fixed output path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./youtube-s2rpl.py --channels=900 --videos=500 subs.json | tee ~/tube-$(date +%s)-size_500-playlist.m3u
+./youtube-s2rpl.py --channels=900 --videos=500 subs.json | tee ~/tube/$(date +%s)-size_500-playlist.m3u
