950d882bdfed8adca162dec9480c96505cdfbffe
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2019-12-07 20:07:57 +0700

    Actually fixed -y flag

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./sm2p.py --channels=900 --videos=555 -b bitchute.html -p subs.json | tr -d '%' | tee ~/tube/$(date +%s)-size_555-playlist.m3u
+./sm2p.py --channels=900 --videos=555 -b bitchute.html -y subs.json | tr -d '%' | tee ~/tube/$(date +%s)-size_555-playlist.m3u
