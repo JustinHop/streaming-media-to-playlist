76cbfb93cefddde5f522e0240da298a5a1f8a818
Justin Hoppensteadt <justinrocksmadscience@gmail.com>
2020-03-11 02:33:09 +0700

    Added runcast

--- runcast.sh
+++ runcast.sh
@@ -6,6 +6,13 @@ VIDEOS=${1:-250}
 DATE=$(date +%s)
 FILE=~/tube/$DATE-size_$VIDEOS-playlist.m3u
 
+if [ ! -d ~/tube ] ; then
+    mkdir ~/tube
+fi
+
 #./sm2p-par.py --channels=1100 --videos=$VIDEOS -b bitchute.html -y subs.json | tr -d '%' | tee $FILE
 ./sm2p-par.py --channels=1100 --videos=$VIDEOS -y subs.json | tr -d '%' | tee $FILE
 castadd $FILE
+
+cd ~/tube
+ln -svf $FILE ./latest.m3u
